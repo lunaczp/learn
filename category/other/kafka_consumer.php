9f9f055be0ccc2ffdb0f940b910289a4180f596b
lunaczp <moon.zhp@gmail.com>
2018-08-31 16:58:00 +0800

    test kafka-php

--- category/other/kafka_consumer.php
+++ category/other/kafka_consumer.php
@@ -11,6 +11,7 @@
 		$rk->addBrokers("127.0.0.1");
 		$topic = $rk->newTopic("test", $cf);
 		while (true) {
+			$t1 = microtime(true);
 			$topic->consumeStart(0, RD_KAFKA_OFFSET_STORED);
 			$msg = $topic->consume(0, 1000);
 			var_dump($msg);
@@ -21,7 +22,10 @@
 				echo $msg->payload, "\n";
 			}
 			$topic->consumeStop(0);
-			sleep(1);
+			$t2 = microtime(true);
+			$int = floor(($t2 - $t1) * 1000);
+			echo $int.PHP_EOL;
+			//sleep(1);
 		}
 	} catch (Exception $e) {
 		echo $e->getMessage();
---
+++ category/other/kafka_produce_one.php
@@ -0,0 +1,28 @@
+<?php
+	date_default_timezone_set("Asia/Shanghai");
+	$t1 = microtime(true);
+	try {
+		$rcf = new RdKafka\Conf();
+
+		//$rcf->set('socket.timeout.ms', 50);
+		//$rcf->set('socket.blocking.max.ms', 50);
+		//$rcf->set('internal.termination.signal', SIGIO);
+		//pcntl_sigprocmask(SIG_BLOCK, [SIGIO]);
+
+		$rcf->set('group.id', 'test');
+		$cf = new RdKafka\TopicConf();
+		$cf->set('offset.store.method', 'broker');
+		$cf->set('auto.offset.reset', 'smallest');
+
+		$rk = new RdKafka\Producer($rcf);
+		$rk->setLogLevel(LOG_DEBUG);
+		$rk->addBrokers("127.0.0.1");
+		$topic = $rk->newTopic("test", $cf);
+		$topic->produce(0,0,'test' . "one".date("Y-m-d H:i:s"));
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+	$t2 = microtime(true);
+
+	$int = ($t2-$t1)*1000;
+	echo $int;
